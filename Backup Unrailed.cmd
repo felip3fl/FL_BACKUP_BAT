a09013939c7ee03bcfbe2961e4f10b1a95d68359
Felip3FL <felip3.fl@gmail.com>
2023-11-07 13:30:11 -0300

    Replaced Fixed address to userprofile

--- Backup Unrailed.cmd
+++ Backup Unrailed.cmd
@@ -5,10 +5,10 @@ For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
 set mydate=%date:~6,4%%date:~3,2%%date:~0,2%
 cd "C:\Program Files\7-Zip\"
 
-set destination="C:\Users\Felipe\OneDrive\Documents\Backup\Games\Unrailed\"
+set destination="%userprofile%\OneDrive\Documents\Backup\Games\Unrailed\"
 
 rem "Backup save-game"
-set origin=""C:\Users\Felipe\AppData\Local\Daedalic Entertainment GmbH\Unrailed\GameState\AllPlayers\""
+set origin=""%userprofile%\AppData\Local\Daedalic Entertainment GmbH\Unrailed\GameState\AllPlayers\""
 set fileName="unrailed_save_game_backup.7z"
 
 .\7z.exe a -t7z %destination%temp.7z "%origin%*" -mx=9
