054bb6f0d520d7558d51a2edc9ba67225fec6696
Felip3FL <felip3.fl@gmail.com>
2023-11-10 14:02:21 -0300

    Fix backup script

--- Backup Documents Images.cmd
+++ Backup Documents Images.cmd
@@ -4,8 +4,8 @@ rem "v 23.01.14.1"
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
 set mydate=%date:~6,4%%date:~3,2%%date:~0,2%
 
-set destination="C:\Users\Felipe\Desktop\"
-set origin="C:\Users\Felipe\OneDrive\Documents\"
+set destination="%userprofile%\Desktop\"
+set origin="%userprofile%\OneDrive\Documents\"
 set fileName="Documents_%mydate%_%mytime%.7z"
 cls 
 
@@ -18,7 +18,7 @@ ren %destination%temp.7z %fileName%
 
 
 
-set origin="C:\Users\Felipe\OneDrive\Pictures\"
+set origin="%userprofile%\OneDrive\Pictures\"
 set fileName="Images_%mydate%_%mytime%.7z"
 cls
 
@@ -27,7 +27,7 @@ cls
 del %destination%%fileName%
 ren %destination%temp.7z %fileName%
 
-set origin="C:\Users\Felipe\AppData\Local\Microsoft\OneNote\"
+set origin="%userprofile%\AppData\Local\Microsoft\OneNote\"
 set fileName="Onenote_%mydate%_%mytime%.7z"
 cls
 
