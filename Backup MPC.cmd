f5972029d494e53058803039acb1f489ca3bd8a1
Felip3FL <felip3.fl@gmail.com>
2023-11-12 19:55:05 -0300

    Fix MPC backup script

--- Backup MPC.cmd
+++ Backup MPC.cmd
@@ -4,9 +4,9 @@ rem "v 22.07.10.1"
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
 set mydate=%date:~6,4%%date:~3,2%%date:~0,2%
 
-set destination="C:\Users\Felipe\OneDrive\Documents\Backup\MPC\"
-set originBE="C:\Users\Felipe\AppData\Roaming\MPC-BE\mpc-be64.ini"
-@REM set originHC="C:\Users\Felipe\AppData\Roaming\MPC-BE\mpc-be64.ini"
+set destination="%userprofile%\OneDrive\Documents\Backup\MPC\"
+set originBE="%userprofile%\AppData\Roaming\MPC-BE\mpc-be64.ini"
+@REM set originHC="%userprofile%\AppData\Roaming\MPC-BE\mpc-be64.ini"
 set fileNameBE="mpc_be_backup.7z"
 @REM set fileNameHC="mpc_hc_backup.7z"
 cls 
