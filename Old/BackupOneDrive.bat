18de48174efe817c189404653741045fb6cdae71
Felip3FL <felip3.fl@gmail.com>
2023-12-04 19:13:21 -0300

    Update address

--- Old/BackupOneDrive.bat
+++ Old/BackupOneDrive.bat
@@ -1,12 +1,12 @@
 REM NINTENDO WII U - CEMU
-Robocopy "C:\Nintendo Wii U\CEMU\mlc01\usr\save"        "C:\Users\felip\OneDrive\Documents\My Games\CEMU\mlc01\usr\save" /mir /z
-Robocopy "C:\Nintendo Wii U\CEMU\mlc01\sys"             "C:\Users\felip\OneDrive\Documents\My Games\CEMU\mlc01\sys" /mir /z
-Robocopy "C:\Nintendo Wii U\CEMU\controllerProfiles"    "C:\Users\felip\OneDrive\Documents\My Games\CEMU\controllerProfiles" /mir /z
-Robocopy "C:\Nintendo Wii U\CEMU\gameProfiles"          "C:\Users\felip\OneDrive\Documents\My Games\CEMU\gameProfiles" /mir /z
+Robocopy "C:\Nintendo Wii U\CEMU\mlc01\usr\save"        "%userprofile%\OneDrive\Documents\My Games\CEMU\mlc01\usr\save" /mir /z
+Robocopy "C:\Nintendo Wii U\CEMU\mlc01\sys"             "%userprofile%\OneDrive\Documents\My Games\CEMU\mlc01\sys" /mir /z
+Robocopy "C:\Nintendo Wii U\CEMU\controllerProfiles"    "%userprofile%\OneDrive\Documents\My Games\CEMU\controllerProfiles" /mir /z
+Robocopy "C:\Nintendo Wii U\CEMU\gameProfiles"          "%userprofile%\OneDrive\Documents\My Games\CEMU\gameProfiles" /mir /z
 
 REM METAL GEAR REVENGEANCE
-Robocopy "C:\Users\felip\Documents\MGR" "C:\Users\felip\OneDrive\Documents\My Games\MGR" /mir /z
+Robocopy "%userprofile%\Documents\MGR" "%userprofile%\OneDrive\Documents\My Games\MGR" /mir /z
 
 REM SETTINGS
-REM Robocopy "C:\Users\felip\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState" "C:\Users\felip\OneDrive\Documents\Settings\Windows Terminal" /mir /z
+REM Robocopy "%userprofile%\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState" "%userprofile%\OneDrive\Documents\Settings\Windows Terminal" /mir /z
 
