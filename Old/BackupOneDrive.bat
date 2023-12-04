6b8afdfe3ebbfc873ede8ce8ab60e3c8a8971507
Felip3FL <felip3.fl@gmail.com>
2023-12-04 19:12:22 -0300

    Add old scripts

---
+++ Old/BackupOneDrive.bat
@@ -0,0 +1,12 @@
+REM NINTENDO WII U - CEMU
+Robocopy "C:\Nintendo Wii U\CEMU\mlc01\usr\save"        "C:\Users\felip\OneDrive\Documents\My Games\CEMU\mlc01\usr\save" /mir /z
+Robocopy "C:\Nintendo Wii U\CEMU\mlc01\sys"             "C:\Users\felip\OneDrive\Documents\My Games\CEMU\mlc01\sys" /mir /z
+Robocopy "C:\Nintendo Wii U\CEMU\controllerProfiles"    "C:\Users\felip\OneDrive\Documents\My Games\CEMU\controllerProfiles" /mir /z
+Robocopy "C:\Nintendo Wii U\CEMU\gameProfiles"          "C:\Users\felip\OneDrive\Documents\My Games\CEMU\gameProfiles" /mir /z
+
+REM METAL GEAR REVENGEANCE
+Robocopy "C:\Users\felip\Documents\MGR" "C:\Users\felip\OneDrive\Documents\My Games\MGR" /mir /z
+
+REM SETTINGS
+REM Robocopy "C:\Users\felip\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState" "C:\Users\felip\OneDrive\Documents\Settings\Windows Terminal" /mir /z
+
