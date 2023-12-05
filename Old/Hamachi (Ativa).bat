0537624a39c87c19f73ae3884159ae9fab778da2
Felip3FL <felip3.fl@gmail.com>
2023-12-05 13:47:09 -0300

    Add new script Hamachi

---
+++ Old/Hamachi (Ativa).bat
@@ -0,0 +1,14 @@
+﻿ECHO OFF
+cls
+
+echo Iniciando LogMeIn Hamachi Tunneling Engine
+echo.
+net start Hamachi2Svc
+
+echo Abilitando conexao Hamachi
+echo.
+netsh int set interface Hamachi enabled
+
+echo Chamando interface Hamachi
+echo.
+"C:\Program Files (x86)\LogMeIn Hamachi\hamachi-2-ui.exe"
---
+++ Old/Hamachi (Desativa).bat
@@ -0,0 +1,10 @@
+﻿ECHO OFF
+cls
+
+echo Parando LogMeIn Hamachi Tunneling Engine
+echo.
+net stop Hamachi2Svc
+
+echo Desabilitando conexao Hamachi
+echo.
+netsh int set interface Hamachi disabled
