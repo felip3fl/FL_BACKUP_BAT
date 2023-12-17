ab6e4e6de88e816df3c0d8eaa675b6fbe6fcb50c
felip3fl <felip3.fl@gmail.com>
2023-12-17 19:49:58 -0300

    Fix backup mgr

--- Backup Metal Gear Rising.cmd
+++ Backup Metal Gear Rising.cmd
@@ -4,14 +4,16 @@ rem "v 22.07.10.1"
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
 set mydate=%date:~6,4%%date:~3,2%%date:~0,2%
 
-set destination=""%userprofile%\OneDrive\Documents\Backup\Games\Metal gear rising\""
+set destination="%userprofile%\OneDrive\Documents\Backup\Games\Metal gear rising\"
 set origin="%userprofile%\Documents\MGR\"
-set fileName="metal_gear_rising_backup.7z"
+set fileName="metal_gear_rising_backup_%mydate%_%mytime%.7z"
 cls 
 
 cd "C:\Program Files\7-Zip\"
 
 .\7z.exe a -t7z %destination%temp.7z "%origin%*" -mx=9
  
-del %destination%%fileName%
+rem del %destination%%fileName%
 ren %destination%temp.7z %fileName%
+
+pause
